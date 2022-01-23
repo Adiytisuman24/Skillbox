7d8e518102b26f9f7b3ceb527fbf9bbb645e82ab
Adiyti Suman <94002154+Adiytisuman24@users.noreply.github.com>
2022-01-23 20:22:47 +0530

    website creation

---
+++ skillbox5.js
@@ -0,0 +1,13 @@
+//initial deploy
+const Migrations = artifacts.require("Migrations");
+
+module.exports = function(deployer) {
+  deployer.deploy(Migrations);
+};
+
+//deploy contracts
+const Migrations = artifacts.require("Migrations");
+
+module.exports = function(deployer) {
+  deployer.deploy(Migrations);
+};
